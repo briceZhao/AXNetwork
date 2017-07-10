58e51523475947f958ef009336f52f0ed2a22700
briceZhao <zx_brice@126.com>
2017-07-10 12:22:37 +0800

    Rename

--- AXNetwork/Pods/Target Support Files/Pods-AXNetwork/Pods-AXNetwork-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AXNetwork : NSObject
-@end
-@implementation PodsDummy_Pods_AXNetwork
-@end
---
+++ ZHNewsDemo/Pods/Target Support Files/Pods-ZHNewsDemo/Pods-ZHNewsDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZHNewsDemo : NSObject
+@end
+@implementation PodsDummy_Pods_ZHNewsDemo
+@end
